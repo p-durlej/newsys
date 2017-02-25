7129219be6b8b6a52ee679010add7f5aed1c6af9
Piotr Durlej <noreply@durlej.net>
2017-02-25 03:20:17 +0100

    clock: update the version number

--- cmd/bin/aclock.c
+++ cmd/bin/aclock.c
@@ -275,7 +275,7 @@ int on_close(struct form *f)
 
 static void about_click(struct menu_item *mi)
 {
-	dlg_about7(main_form, NULL, "Clock v1", SYS_PRODUCT, SYS_AUTHOR, SYS_CONTACT, "clock.pnm");
+	dlg_about7(main_form, NULL, "Clock v1.3", SYS_PRODUCT, SYS_AUTHOR, SYS_CONTACT, "clock.pnm");
 }
 
 int main()
