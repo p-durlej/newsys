1c78e8640e761c6e5a8c3eebdf943a4eae190609
Piotr Durlej <noreply@durlej.net>
2017-04-21 23:39:47 +0200

    tcc: produce an error message instead of calling abort() when a floating-point number is encountered

--- cmd/tcc/slib/strtold.c
+++ cmd/tcc/slib/strtold.c
@@ -1,22 +1,27 @@
 
 #include <stdlib.h>
 
+static void nofloat(void)
+{
+	tcc_error("floating-point support is disabled");
+}
+
 long double strtold(const char *nptr, char **endptr)
 {
-	abort();
+	nofloat();
 }
 
 double strtod(const char *nptr, char **endptr)
 {
-	abort();
+	nofloat();
 }
 
 float strtof(const char *nptr, char **endptr)
 {
-	abort();
+	nofloat();
 }
 
 double ldexp(double x, int exp)
 {
-	abort();
+	nofloat();
 }
