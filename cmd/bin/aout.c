47cc7136ffb93b51ab28f4ee1a89f9a33b8e9583
Piotr Durlej <noreply@durlej.net>
2017-10-08 02:01:27 +0200

    aout: prevent exec loops

--- cmd/bin/aout.c
+++ cmd/bin/aout.c
@@ -1,9 +1,19 @@
 
+#include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
 #include <err.h>
 
 int main(int argc, char **argv)
 {
+	struct stat st1, st2;
+	
+	if (!stat("/bin/aout", &st1) && !stat("a.out", &st2) && st1.st_dev == st2.st_dev && st1.st_ino == st2.st_ino)
+	{
+		errno = EPERM;
+		err(127, "a.out");
+	}
+	
 	execv("a.out", argv);
 	err(127, "a.out");
 }
